511b37e656f4b768ebcc13a99b29954e87ad3075
sarah pai <pai.sarah89@gmail.com>
2018-11-20 21:29:58 -0500

    Done.

--- index.js
+++ index.js
@@ -4,4 +4,25 @@ const questions = [
   {questionText: "Goldfish only have a memory of about three seconds", answer: false}
 ]
 
-let question;
+let question = questions[0];
+
+const questionContainer = document.querySelector(".question-container")
+
+function appendQuestion(question){
+  const newComment = document.createElement("p");
+  newComment.innerText = (question.questionText)
+  questionContainer.appendChild(newComment)
+}
+
+function askQuestionThen(time){
+  return new Promise((resolve, reject) => {
+    setTimeout(function(){
+      let value = appendQuestion
+      resolve(value)
+    }),time
+  })
+}
+
+// function removeQuestion {
+//
+// }
