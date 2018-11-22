53b40210251e8286090c52ab9052488af95b1ae9
sarah pai <pai.sarah89@gmail.com>
2018-11-22 13:30:06 -0500

    Done.

--- index.js
+++ index.js
@@ -4,25 +4,53 @@ const questions = [
   {questionText: "Goldfish only have a memory of about three seconds", answer: false}
 ]
 
-let question = questions[0];
+let question;
 
 const questionContainer = document.querySelector(".question-container")
+const buttons = document.querySelectorAll('.true-false-list .btn')
+const askAwayBtn = document.querySelector("a.btn")
 
 function appendQuestion(question){
-  const newComment = document.createElement("p");
-  newComment.innerText = (question.questionText)
-  questionContainer.appendChild(newComment)
+  return questionContainer.innerHTML = question.questionText
 }
 
+
 function askQuestionThen(time){
-  return new Promise((resolve, reject) => {
-    setTimeout(function(){
-      let value = appendQuestion
-      resolve(value)
-    }),time
+  return new Promise(function(resolve) {
+    question = questions[0]
+    appendQuestion(question)
+    setTimeout(function() {
+      resolve(question)
+    },time)
   })
 }
 
-// function removeQuestion {
-//
-// }
+function removeQuestion() {
+  questionContainer.innerHTML = "";
+}
+
+function askQuestionThenRemoveQuestion() {
+  return new Promise(function(resolve){
+    return appendQuestion(question)
+  },1000)
+}
+
+function trueAndFalseButtons() {
+  buttons[0].innerText = "TRUE"
+  buttons[1].innerText = "FALSE"
+  return buttons
+}
+
+function toggleTrueAndFalseButtons() {
+  trueAndFalseButtons().forEach(function(btn){
+    btn.classList.toggle('hide')
+  })
+}
+
+function displayQuestionOnClick() {
+  return askAwayBtn.addEventListener("click", function(){
+    return toggleTrueAndFalseButtons();
+    askQuestionThenRemoveQuestion(5000)
+  })
+}
+displayQuestionOnClick()
